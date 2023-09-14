9069b5ba878f3c4ff5604655dfb2afb074c3cd6c
imran <zunaeede@gmail.com>
2023-09-14 10:03:52 +0600

    main - add comment to better understand

--- 1_arraymap_basic.js
+++ 1_arraymap_basic.js
@@ -1,5 +1,5 @@
 const values = [2, 4, 54, 8, 10]
 
-const newValues = values.map(value => value + 2)
+const newValues = values.map(value => value + 2) // we add 2 in every array
 
 document.write(newValues);
