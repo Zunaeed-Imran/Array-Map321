f24b31e11422415dab0d1203f3154e6bb4025a10
imran <zunaeede@gmail.com>
2023-09-14 13:11:50 +0600

    main - add a comment

--- 2_array_map_index.js
+++ 2_array_map_index.js
@@ -2,5 +2,5 @@ const values = [12, 9, 36, 55, 2]
 
 const newValues = values.map((value, index) => {
     newValue = value + 2
-    document.write( 'new value: ' + newValue + ' ,index number: ' + (index + 1)) // index will পর্যায়ক্রমিকভাবে নাম্বারিং করবে
+    document.write( 'new value: ' + newValue + ' ,index number: ' + (index + 1)) // index will পর্যায়ক্রমিকভাবে.নাম্বারিং করবে
 })
