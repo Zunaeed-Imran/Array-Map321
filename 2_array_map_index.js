720b7e3c9c6666025677aa32c1f9c07184f604ff
imran <zunaeede@gmail.com>
2023-09-14 13:09:57 +0600

    main - index array

--- 2_array_map_index.js
+++ 2_array_map_index.js
@@ -1,4 +1,6 @@
-const var1 = [12, 9, 36, 55, 2]
-const var2 = var1.map(values => values+1);
+const values = [12, 9, 36, 55, 2]
 
-document.write(values);
+const newValues = values.map((value, index) => {
+    newValue = value + 2
+    document.write( 'new value: ' + newValue + ' ,index number: ' + (index + 1)) // index will পর্যায়ক্রমিকভাবে নাম্বারিং করবে
+})
--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Array-practice in javaScript
 1. basic array map ptactice
 - in that example we add 2 in every array value
 2. array map index example practice
-- 
+- index will পর্যায়ক্রমিকভাবে নাম্বারিং করবে
