e68784b54cbc32b59f454a60488a373925ec5bd8
imran <zunaeede@gmail.com>
2023-09-17 09:40:46 +0600

    practice - practice sum of array -  in function

--- 6_practice.js
+++ 6_practice.js
@@ -3,5 +3,10 @@
 const arr = [1, 6, 99, 36, 5, 85];
 
 function sumArr(arr) {
-  
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += i;
+  }
+  return sum;
 }
+document.write(sumArr(arr));
