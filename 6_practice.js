6bc02ea2da57839bf03aa7c5879dd1db4b3dc508
imran <zunaeede@gmail.com>
2023-09-17 09:52:20 +0600

    practice - some important comment

--- 6_practice.js
+++ 6_practice.js
@@ -1,10 +1,14 @@
 // using function sum of array
+// i can understand from main branch 12 array function
+// i practice for to understand for difference between array[i] and i;
 
 const arr = [1, 6, 99, 36, 5, 85];
 
 function sumArr(arr) {
   let sum = 0;
   for (let i = 0; i < arr.length; i++) {
+    document.write(i + ' ');
+    sum += i + '<br>';
     sum += arr[i];
   }
   return sum;
