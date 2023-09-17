3793408b682f2009505359620c2f48029856f0d1
imran <zunaeede@gmail.com>
2023-09-17 17:43:10 +0600

    practice - try 5

--- 11_practice.js
+++ 11_practice.js
@@ -1,13 +1,11 @@
 // trying to make a programm that can input number will summ of it
 
-
 let num1 = prompt('Enter a number');
 let num2 = num1.split(',');
+let num3 = num2.join();
 
 let sum = 0;
-for (let n of num2) {
-  // sum += sum+n; or
-  sum = sum + n;
+for (let n of num3) {
+  sum += sum + n;
 }
 document.write(sum);
-
