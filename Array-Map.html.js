be60bce27e96a21b247e2ca2d85b98c3a1377a01
imran <zunaeede@gmail.com>
2023-09-17 17:22:14 +0600

    practice - array map - listion array - array filter

--- Array-Map.html.js
+++ Array-Map.html.js
@@ -2,8 +2,8 @@ const numbers = [1, -1, 2, 3];
 
 const filtered = numbers.filter(n => n >= 0);
 
-const items =filtered.map(n => '<li>' + n + '</li>')
+const items = filtered.map(n => '<li>' + n + '</li>');
 
 const html = items.join('');
 
-console.log(html)
+document.write(html);
