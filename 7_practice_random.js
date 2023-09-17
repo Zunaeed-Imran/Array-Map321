afe253faaaed32d468a4706288d90247966ccaca
imran <zunaeede@gmail.com>
2023-09-17 10:19:18 +0600

    practice - practice with array function - function parameter change - make a good practice

--- 7_practice_random.js
+++ 7_practice_random.js
@@ -1,4 +1,13 @@
 //  just a random practices
 
-const array = [1,5,9,63,785,4,5];
+const arr = [1, 5, 9, 63, 785, 4, 5];
 
+function mytest3(a) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+
+document.write(mytest3(arr));
