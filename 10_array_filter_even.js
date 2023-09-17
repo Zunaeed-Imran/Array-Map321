42ae589e3258e302b539cf64a876e31d8300e760
imran <zunaeede@gmail.com>
2023-09-17 08:05:50 +0600

    main - array even number filter - add comment

--- 10_array_filter_even.js
+++ 10_array_filter_even.js
@@ -1,1 +1,7 @@
-const arr = [1, 2, 3, 9, 6, 44, 58, 47, 58, 59, 61, 71];
+// example practice with even number in array filter
+
+const arr = [0, 1, 10, 3, 9, 6, 44, 58, 47, 58, 59, 61, 71];
+
+const result = arr.filter(even => even % 2 === 0);
+
+document.write(result);
