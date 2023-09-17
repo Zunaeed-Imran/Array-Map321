428359dd4a5f6d69f23648b7fcaad352fc63547b
imran <zunaeede@gmail.com>
2023-09-17 17:16:59 +0600

    practice - using for loop

--- 11_practice.js
+++ 11_practice.js
@@ -1,9 +1,13 @@
 // trying to make a programm that can input number will summ of it
 
+
 let num1 = prompt('Enter a number');
 let num2 = num1.split(',');
-let num3 = num2.join();
 
-let result = num2.reduce((a, c) => a + c);
+let sum = 0;
+for (let n of num2) {
+  // sum += sum+n; or
+  sum = sum + n;
+}
+document.write(sum);
 
-document.write(result);
