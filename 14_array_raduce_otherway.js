89df2ad5f9673e93563d99766c6e9afbfe5d10d1
imran <zunaeede@gmail.com>
2023-09-18 10:35:31 +0600

    main - array reduce

--- 14_array_raduce_otherway.js
+++ 14_array_raduce_otherway.js
@@ -1,8 +1,17 @@
 const users = [
-  { firstName: 'akshay', lastName: 'kumar', age: 23 },
-  { firstName: 'Sharook', lastName: 'Khan', age: 60 },
-  { firstName: 'Salman', lastName: 'Khan', age: 70 },
-  { firstName: 'Amir', lastName: 'khan', age: 50 },
+  { firstName: 'akshay', lastName: 'kumar', age: 26 },
+  { firstName: 'Sharook', lastName: 'Khan', age: 75 },
+  { firstName: 'Salman', lastName: 'Khan', age: 50 },
+  { firstName: 'Amir', lastName: 'khan', age: 26 },
 ];
 
-const resulu = users.reduce();
+const resulu = users.reduce(function (acc, curr) {
+  if (acc[curr.age]) {
+    acc[curr.age] = ++acc[curr.age];
+  } else {
+    acc[curr.age] = 1;
+  }
+  return acc;
+}, {});
+
+document.write(resulu);
